a3b3a2685ecd468213c7e4bfa002582dddcd5607
Eddy Rivera <eddy.rivera.echegaray@gmail.com>
2020-03-08 19:46:29 -0300

    Remove console.log

--- src/pages/collection/Collection.container.js
+++ src/pages/collection/Collection.container.js
@@ -2,11 +2,8 @@ import Collection from "./Collection";
 import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
-const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps);
-  return {
-    collection: selectCollection(ownProps.match.params.categoryId)(state)
-  };
-};
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.categoryId)(state)
+});
 
 export default connect(mapStateToProps)(Collection);
